50923779e6da0d5d08819b1acabdda37d85a9afb
Justin Perez <38331991+jperez-1@users.noreply.github.com>
2021-06-09 20:36:37 -0400

    Function Declarations & Expressions

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,41 +1,47 @@
-const color = 'yellow';
+// FUNCTION DECLARATIONS
 
-switch(color){
-    case 'red':
-        console.log('Color is red');
-        break;
-    case 'blue':
-        console.log('Color is blue');
-        break;
-    default:
-        console.log('Color is not red or blue');
-        break;
+function greet(firstName = 'Justin', lastName = 'Perez'){
+    // if(typeof firstName === 'undefined'){firstName = 'Justin'}
+    // if(typeof lastName === 'undefined'){lastName = 'Perez'}
+    //console.log('Hello');
+    return 'Hello ' + firstName + ' ' + lastName;
+};
+
+// console.log(greet());
+
+// FUNCTION EXPRESSIONS
+
+const square = function(x = 3){
+    return x * x;
+};
+
+// console.log(square());
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS - IIFEs
+
+// (function(){
+//     console.log('IIFE Ran..');
+// })();
+
+// (function(name){
+//     console.log('Hello ' + name);
+// })('Justin');
+
+// PROPERTY METHODS
+
+const todo = {
+    add: function(){
+        console.log('Add todo..')
+    },
+    edit: function(id){
+        console.log(`Edit todo ${id}`);
+    }
+};
+
+todo.delete = function(){
+    console.log('Delete todo...');
 }
 
-let day;
-
-switch(new Date().getDay()){
-    case 0:
-        day = 'Sunday';
-        break;
-    case 1:
-        day = 'Monday';
-        break;
-    case 2:
-        day = 'Tuesday';
-        break;
-    case 3:
-        day = 'Wednesday';
-        break;
-    case 4:
-        day = 'Thursday';
-        break;
-    case 5:
-        day = 'Friday';
-        break;
-    case 6:
-        day = 'Saturday';
-        break;
-}
-
-console.log(`Today is ${day}`);
+todo.add();
+todo.edit(22);
+todo.delete();
